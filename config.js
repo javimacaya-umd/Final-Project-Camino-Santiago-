4a66e1da0e5a1a877ef78d8a7b79f6c41ad8cab0
javimacaya-umd <macay004@d.umn.edu>
2020-12-08 01:08:24 -0600

    Include photo and narrative to chaper 1-A
    Include photo and narrative to chaper 1-A

--- config.js
+++ config.js
@@ -13,10 +13,30 @@ var config = {
     footer: '', // for citations
     chapters: [
         {
-            id: 'chapter_1',
-            title: 'Overview of The Way of Saint James - The Northern Way',
-            image: './path/to/image/source.png',
-            description: 'Description of The Way of Saint James - The Northern Way',
+            id: 'chapter_1_A',
+            title: 'The Way of Saint James - The Northern Way',
+            image: '/images/chapter-1-overview.jpg',
+            description: 'The Way of St. James (El Camino de Santiago), is a pilgrimage of Medieval Origin to the Cathedral of Santiago de Compostela in Galicia, in the Northwest of Spain. Legend has it that the remains of the Apostle Saint James the Great were buried here and discovered by a shepherd in the 9th century. The city is, in fact, named after the apostle: Santiago de Compostela means St James of the Field of Stars.The Camino de Santiago has been listed as UNESCO World Heritage for its important role encouraging cultural exchanges between people from all over Europe and the world for many centuries. Today, more than a pilgrimage, the Camino de Santiago, is a unique experience and trip of a lifetime and the routes attract thousands of people from all over the world.',
+            scrollDown: 'scroll-arrow.png',
+            location: {
+                center: [2.305, 40.602],
+                zoom: 5.50,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                 // {
+                 //    layer: 'layer-name',
+                 //    opacity: 1
+                 // }
+            ],
+            onChapterExit: [
+                 // {
+                 //     layer: 'layer-name',
+                 //     opacity: 0
+                 // }
+            ]
+        },
             scrollDown: 'scroll-arrow.png',
             location: {
                 center: [2.305, 40.602],
